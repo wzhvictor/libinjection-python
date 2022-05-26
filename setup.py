87e6375bb16b0150abd2ad42acf94183ac0e74ff
John G G <john.gerlock@gmail.com>
2022-05-26 19:02:05 +0000

    Update setup.py to allow install without cython, add pyproject.toml to specify cython as build-dep

--- setup.py
+++ setup.py
@@ -1,44 +1,54 @@
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
 
-source_files = [
-    'libinjection/libinjection.pyx',
-    'libinjection/src/libinjection_sqli.c',
-    'libinjection/src/libinjection_html5.c',
-    'libinjection/src/libinjection_xss.c'
+USE_CYTHON = False
+
+try:
+    from Cython.Build import cythonize
+
+    USE_CYTHON = True
+except ImportError:
+    pass
+
+ext = ".pyx" if USE_CYTHON else ".c"
+
+extensions = [
+    Extension(
+        name="libinjection",
+        sources=[
+            "libinjection/libinjection" + ext,
+            "libinjection/src/libinjection_sqli.c",
+            "libinjection/src/libinjection_html5.c",
+            "libinjection/src/libinjection_xss.c",
+        ],
+        include_dirs=["libinjection/src"],
+        library_dirs=["libinjection/src"],
+    )
 ]
-cmd_class = {'build_ext': build_ext}
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
 
 setup(
-    name='libinjection-python',
-    version='1.1.4',
-    author='wzhvictor',
-    author_email='wzhvictor@outlook.com',
-    url='https://github.com/wzhvictor/libinjection-python',
-    description='Libinjection Python Wrapper',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    name="libinjection-python",
+    version="1.1.6",
+    author="wzhvictor",
+    author_email="wzhvictor@outlook.com",
+    url="https://github.com/wzhvictor/libinjection-python",
+    description="Libinjection Python Wrapper",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: Unix',
-        'Programming Language :: Cython',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Cython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     packages=[],
     include_package_data=True,
-    cmdclass=cmd_class,
-    ext_modules=[Extension(
-        name='libinjection',
-        sources=source_files,
-        include_dirs=['libinjection/src'],
-        library_dirs=['libinjection/src'])
-    ],
-    setup_requires=[
-        'setuptools>=38.3.0',
-        'Cython>=0.23'
-    ]
+    ext_modules=extensions,
+    setup_requires=["setuptools>=38.3.0", "Cython>=0.23"],
 )
